93987894b0cdedf6d605aa52951da962e56070b1
Abdul Aziz Dal <95477937+Abdul-Aziz-dal@users.noreply.github.com>
2025-05-22 17:13:21 +0500

    updated code

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'client_id' => 'your client id',
+    'client_secret' => 'your cient secret',
+    'redirect_uri' => 'url',
+    'scopes' => 'openid profile email w_member_social'
+];
